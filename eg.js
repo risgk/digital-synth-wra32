6f5e7e2fd7fde1f27eaa151f74e58b5c35e6a7c9
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-10-26 13:46:35 +0900

    Implementing EG

--- eg.js
+++ eg.js
@@ -6,11 +6,11 @@ var EG = function() {
   const STATE_IDLE    = 4;
 
   this.setAttackTime = function(attackTime) {
-    this.attackSpeed = 0;
+    this.attackTime = attackTime;
   }
 
   this.setDecayTime = function(decayTime) {
-    this.decaySpeed = 0;
+    this.decayTime = decayTime;
   }
 
   this.setSustainLevel = function(sustainLevel) {
@@ -19,12 +19,10 @@ var EG = function() {
 
   this.noteOn = function() {
     this.state = STATE_ATTACK;
-    this.count = 0;
   }
 
   this.noteOff = function() {
     this.state = STATE_RELEASE;
-    this.count = 0;
   }
 
   this.soundOff = function() {
@@ -32,11 +30,56 @@ var EG = function() {
   }
 
   this.clock = function() {
-    return 1;
+    var at = this.attackTime;
+    if (at > 120) {
+      at = 120;
+    }
+    var ar = 0.9999; // TODO
+    var dt = this.decayTime;
+    if (dt > 120) {
+      dt = 120;
+    }
+    var dr = 0.9999; // TODO
+    var sl = this.sustainLevel / 120;
+    if (sl > 1) {
+      sl = 1;
+    }
+
+    switch (this.state) {
+    case STATE_ATTACK:
+      this.level = 2 - ((2 - this.level) * ar);
+      if (this.level >= 1) {
+        this.state = STATE_DECAY;
+        this.level = 1;
+      }
+      break;
+    case STATE_DECAY:
+      this.level = this.level * dr;
+      if (this.level <= sl) {
+        this.state = STATE_SUSTAIN;
+        this.level = sl;
+      }
+      break;
+    case STATE_SUSTAIN:
+      this.level = sl;
+      break;
+    case STATE_RELEASE:
+      this.level = this.level * dr;
+      if (this.level <= 1 / 1024) {
+        this.state = STATE_IDLE;
+        this.level = 0;
+      }
+      break;
+    case STATE_IDLE:
+      this.level = 0;
+      break;
+    }
+
+    return this.level;
   }
 
-  this.attackSpeed  = 255;
-  this.decaySpeed   = 255;
+  this.attackTime   = 0;
+  this.decayTime    = 0;
   this.sustainLevel = 127;
   this.level        = 0;
 };
