50e00610c3db6f1e7303de5547ded6861f42572b
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2014-12-23 23:30:41 +0900

    Revert "Introduce new VCO logic based on FFT"
    This reverts commit 0d3dd1491f23be8239626d125196be610533bca0.

--- vco.js
+++ vco.js
@@ -5,50 +5,50 @@ var VCO = function() {
 
   that = this;
 
-  var generateWaveTable = function(waveTables, originalWaveTable) {
+  var generateWaveTable = function(waveTables, f) {
     for (var m = 0; m <= Math.floor((MAX_OVERTONE + 1) / 2) - 1; m++) {
       var waveTable = new Float64Array(SAMPLES_PER_CYCLE);
-      var w = ifft(lpf(fft(originalWaveTable), m));
       for (var t = 0; t < SAMPLES_PER_CYCLE; t++) {
-        waveTable[t] = w[t];
+        var level = 0;
+        for (var k = 1; k <= (m * 2) + 1; k++) {
+          level += f(t, k);
+        }
+        waveTable[t] = level;
       }
       waveTables[m] = waveTable;
     }
   };
 
   this.waveTablesSawtooth = [];
-  this.originalSawtooth = [];
-  for (var t = 0; t < SAMPLES_PER_CYCLE; t++) {
-    this.originalSawtooth[t] = +1 - (2 * (t + 0.5)) / SAMPLES_PER_CYCLE;
-  }
-  generateWaveTable(this.waveTablesSawtooth, this.originalSawtooth);
+  generateWaveTable(this.waveTablesSawtooth, function(t, k) {
+    return (2 / Math.PI) * Math.sin((2 * Math.PI) * (t / SAMPLES_PER_CYCLE) * k) / k;
+  });
 
   this.waveTablesSquare = [];
-  this.originalSquare = [];
-  for (var t = 0; t < SAMPLES_PER_CYCLE; t++) {
-    this.originalSquare[t] = (t + 0.5) < (SAMPLES_PER_CYCLE * 0.5) ? 1 : -1;
-  }
-  generateWaveTable(this.waveTablesSquare, this.originalSquare);
+  generateWaveTable(this.waveTablesSquare, function(t, k) {
+    if (k % 2 == 1) {
+      return (4 / Math.PI) * Math.sin((2 * Math.PI) * (t / SAMPLES_PER_CYCLE) * k) / k;
+    }
+    return 0;
+  });
 
   this.waveTablesTriangle = [];
-  this.originalTriangle = [];
-  for (var t = 0; t < SAMPLES_PER_CYCLE; t++) {
-    if (t < SAMPLES_PER_CYCLE / 4) {
-      this.originalTriangle[t] = (t + 0.5) * (4 / SAMPLES_PER_CYCLE);
-    } else if (t < (SAMPLES_PER_CYCLE * 3) / 4) {
-      this.originalTriangle[t] = 1 - ((t + 0.5) - (SAMPLES_PER_CYCLE * 1) / 4) * (4 / SAMPLES_PER_CYCLE);
-    } else {
-      this.originalTriangle[t] = -1 + ((t + 0.5) - (SAMPLES_PER_CYCLE * 3) / 4) * (4 / SAMPLES_PER_CYCLE);
+  generateWaveTable(this.waveTablesTriangle, function(t, k) {
+    if (k % 4 == 1) {
+      return (8 / Math.pow(Math.PI, 2)) * Math.sin((2 * Math.PI) * (t / SAMPLES_PER_CYCLE) * k) / Math.pow(k, 2);
+    } else if (k % 4 == 3) {
+      return (8 / Math.pow(Math.PI, 2)) * -Math.sin((2 * Math.PI) * (t / SAMPLES_PER_CYCLE) * k) / Math.pow(k, 2);
     }
-  }
-  generateWaveTable(this.waveTablesTriangle, this.originalTriangle);
+    return 0;
+  });
 
   this.waveTablesSine = [];
-  this.originalSine = [];
-  for (var t = 0; t < SAMPLES_PER_CYCLE; t++) {
-    this.originalSine[t] = Math.sin((2 * Math.PI) * ((t + 0.5) / SAMPLES_PER_CYCLE));
-  }
-  generateWaveTable(this.waveTablesSine, this.originalSine);
+  generateWaveTable(this.waveTablesSine, function(t, k) {
+    if (k == 1) {
+      return Math.sin((2 * Math.PI) * (t / SAMPLES_PER_CYCLE));
+    }
+    return 0;
+  });
 
   this.freqTableC4toB4 = [];
   var generatefreqTable = function() {
